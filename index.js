f69f8a1def06b4bc290f2288429c4406b9f9182a
GitHubDev2021 <imusic.kondeaw@gmail.com>
2021-12-26 18:53:19 +0700

    bolierplate canvas

---
+++ index.js
@@ -0,0 +1,27 @@
+// Canvas setup
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+let width = canvas.width = window.innerWidth;
+let height= canvas.height= window.innerHeight;
+
+// Object
+
+
+
+// Animation loop
+
+let fps = 60;
+let lastTime = 0;
+function animate(timestamp) {
+    const deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate);
+
+
+
